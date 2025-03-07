16697d9591d6b359147b22e5409e87b9aea215ff
devMuriloPY <contatomurilo.dev@gmail.com>
2025-03-07 16:47:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.routes import auth, websocket
 
-app = FastAPI(title="Minha API FastAPI")
+app = FastAPI(title="API Portal XML")
 
 # Incluir rotas HTTP
 app.include_router(auth.router, prefix="/auth", tags=["Autenticação"])
